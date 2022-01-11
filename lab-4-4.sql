386da9d1ceb2579f62f8a51827b1454c9d125ba0
Louis <louis.harel@kellogg.northwestern.edu>
2022-01-11 17:36:24 +0000

    Completed 4th ex of 4th lab

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -35,4 +35,9 @@
 -- | Washington Nationals          | Anthony    | Rendon      | 34                   |
 -- +-------------------------------+------------+-------------+----------------------+
 
-
+select teams.name, players.first_name, players.last_name, MAX(stats.home_runs)
+from stats 
+inner join players on players.id = stats.player_id
+inner join teams on teams.id = stats.team_id
+where teams.year = 2019
+group by teams.name;
