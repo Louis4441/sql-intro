efea47ee01954b0690dcbfdb03510eb02aecc1e4
Louis <louis.harel@kellogg.northwestern.edu>
2022-01-11 17:32:08 +0000

    Answered 3rd exercise 4th lab

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -6,4 +6,9 @@
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
 
-
+select teams.name, players.first_name, players.last_name, MAX(stats.home_runs)
+from stats 
+inner join players on players.id = stats.player_id
+inner join teams on teams.id = stats.team_id
+where teams.year = 2019
+limit 2;
